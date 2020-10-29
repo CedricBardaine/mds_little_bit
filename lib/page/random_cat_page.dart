c5aebfbc210551c6de4e63a53ab5cd4d706d4400
Cédric B <cedricbardaine@gmail.com>
2020-10-29 16:54:59 +0100

    refersh goodly the cats

--- lib/page/random_cat_page.dart
+++ lib/page/random_cat_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:mds_little_bit/api/meow_api.dart';
 import 'package:mds_little_bit/model/meow_model.dart';
@@ -8,19 +9,44 @@ class RandomCatPage extends StatefulWidget {
 }
 
 class _RandomCatPageState extends State<RandomCatPage> {
+  MeowModel _meowModel;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Random Cat 🐱")),
-      body: IconButton(
-        icon: Icon(Icons.refresh),
-        onPressed: () async {
-          print('refesh');
-          MeowModel meowModel = await getMeow();
+      appBar: AppBar(
+        title: Text("Random Cat 🐱"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () async {
+              print('refesh');
+              MeowModel newMeowModel = await getMeow();
 
-          print(meowModel.file);
-        },
+              setState(() {
+                _meowModel = newMeowModel;
+              });
+
+              print(_meowModel.file);
+            },
+          ),
+        ],
+      ),
+      body: Row(
+        children: [
+          _buildContent(),
+        ],
       ),
     );
+  }
+
+  Widget _buildContent() {
+    if (_meowModel == null) {
+      return Center(
+        child: Text('snif snif, no cat 😿'),
+      );
+    } else {
+      return CachedNetworkImage(imageUrl: _meowModel.file);
+    }
   }
 }
